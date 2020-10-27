8f72f70dc8ecce2d07a66fe2a4476860b05ed90c
Joaquim Scavone <joaquim@joaquimscavone.com.br>
2020-10-26 21:41:48 -0300

    Colocando estrutura html

--- variaveis.php
+++ variaveis.php
@@ -1,5 +1,8 @@
 <?php
 
+
+
+
 $idade = 15;
 
 printvar($idade);
@@ -22,10 +25,33 @@ printvar($usuario);
 printvar($usuarios);
 
 function printvar($var) {
-    echo "Echo: ". $var."<br/>";
-    echo "Vardump:";
-    var_dump($var);
-    echo "print_r:";
-    print_r($var);
-    echo "<hr/>";
+//    echo "Echo: ". $var."<br/>";
+//    echo "Vardump:";
+//    var_dump($var);
+//    echo "print_r:";
+//    print_r($var);
+//    echo "<hr/>";
 }
+?>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Variavéis</title>
+    </head>
+    <body>
+        <h1>Numerico</h1>
+        
+        <span>$var = 5</span>
+        
+        <hr>
+        <h1>String</h1>
+        
+        <span>$var = "5"</span>
+    </body>
+</html>
